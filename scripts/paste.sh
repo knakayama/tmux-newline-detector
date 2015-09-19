3c98952c1714d9d562738abc20b79ac74fc9800e
knakayama <knakayama.sh@gmail.com>
2015-09-19 13:33:23 +0900

    Add auto remove newline feat

--- scripts/paste.sh
+++ scripts/paste.sh
@@ -8,6 +8,11 @@ if is_osx && is_command_available 'reattach-to-user-namespace'; then
   reattach-to-user-namespace pbpaste | tmux load-buffer -
 fi
 
+# remove newline if one sentence
+if [[ $(tmux show-buffer | hexdump | grep -cE '[[:space:]](0[ad]|0d0a)[[:space:]]?') -eq 1 ]]; then
+  tmux show-buffer | tr -d '\n' | tmux load-buffer -
+fi
+
 if tmux show-buffer | hexdump | grep -qE '[[:space:]](0[ad]|0d0a)[[:space:]]?'; then
   tmux confirm-before -p "Include newline. Paste?(y/n)" "run-shell 'tmux paste-buffer'"
 else
