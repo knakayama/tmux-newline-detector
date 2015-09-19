26bd10edf944c31dbd727faed3a0a48e5b67a962
knakayama <knakayama.sh@gmail.com>
2015-09-19 12:17:03 +0900

    Update regexp

--- scripts/paste.sh
+++ scripts/paste.sh
@@ -8,7 +8,7 @@ if is_osx && is_command_available 'reattach-to-user-namespace'; then
   reattach-to-user-namespace pbpaste | tmux load-buffer -
 fi
 
-if tmux show-buffer | hexdump | grep -qE '[[:space:]](0a|0d|0d0a)[[:space:]]?'; then
+if tmux show-buffer | hexdump | grep -qE '[[:space:]](0[ad]|0d0a)[[:space:]]?'; then
   tmux confirm-before -p "Include newline. Paste?(y/n)" "run-shell 'tmux paste-buffer'"
 else
   tmux paste-buffer
