469904200493b6a4aa8b4ecaaf3be864515407dc
Robin Lee Powell <rlpowell@digitalkingdom.org>
2015-11-18 10:50:55 -0800

    Further improvements to argument handling; now works with both the examples from the README.

--- scripts/paste.sh
+++ scripts/paste.sh
@@ -9,12 +9,12 @@ if is_osx && is_command_available 'reattach-to-user-namespace'; then
 fi
 
 args="$*"
-buffer="0"
+buffer="$(tmux list-buffers -F '#{buffer_name}' | head -n 1)"
 # Grab a buffer selector if one exists
-if [ $(echo "$args" | grep -P '[-]b \S+') ]
+if echo "$args" | grep -P '\s[-]b\s+\S+' >/dev/null 2>&1
 then
-  args="$(echo "$args" | sed -r 's/\s*[-]b \S+\s*/ /')"
-  buffer="$(echo "$args" | sed -r 's/.*\b[-]b \(\S+\)\b*.*/\1/')"
+  buffer="$(echo "$args" | sed -r 's/.*\s+[-]b\s+(\S+).*/\1/')"
+  args="$(echo "$args" | sed -r 's/\s+[-]b\s+\S+\s*/ /')"
 fi
 
 # remove newline if one sentence
@@ -22,11 +22,13 @@ fi
 # wc -l works fine here; test example: 
 # $ echo -e 'foo\nbar\rbaz\r\nqux\n\rwibble\n' | wc -l
 # 5
-if [[ $(tmux show-buffer -b $buffer | wc -l) -eq 1 ]]; then
+if [ "$(tmux show-buffer -b $buffer | wc -l)" -eq 1 ]
+then
   tmux show-buffer -b $buffer | tr -d '\n' | tmux load-buffer -b $buffer -
 fi
 
-if [[ $(tmux show-buffer -b $buffer | wc -l) -gt 0 ]]; then
+if [ "$(tmux show-buffer -b $buffer | wc -l)" -gt 0 ]
+then
   tmux confirm-before -p "This paste contains newlines; paste anyway?(y/n)" "run-shell 'tmux paste-buffer -b $buffer $args'"
 else
   tmux paste-buffer -b $buffer $args
